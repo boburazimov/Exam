6a29ab3f802ef73603b970778be15e8281e3428e
bobur <uzbek.4103@gmail.com>
2022-07-02 21:41:54 +0500

    Second Com

---
+++ src/test/java/uz/dynamic/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.dynamic.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
